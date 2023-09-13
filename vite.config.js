81acd15d927fc76cc1eaf628529ac2606a71d526
Ciprian-hub <cipssul@gmail.com>
2023-09-13 14:29:12 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import Vue from '@vitejs/plugin-vue'
 import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  // resolve: {
-  //   alias: {
-  //     '@': path.resolve(__dirname, 'src')
-  //   }
-  // }
+  build: {
+    target: 'es2015',
+  },
+  plugins: [Vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
 })
