55d298cc62676d1aa038c1b175d3120312255f5d
Ciprian-hub <cipssul@gmail.com>
2023-09-14 14:02:07 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
 import * as path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   build: {
     rollupOptions: {
@@ -17,9 +16,9 @@ export default defineConfig({
   },
   plugins: [Vue()],
   resolve: {
-    // alias: {
-    //   '@': path.resolve(__dirname, 'src')
-    // },
-    // extensions: ['.js', '.json', '.jsx', '.mjs', '.ts', '.tsx', '.vue'],
+    alias: {
+      '@': path.resolve( 'src')
+    },
+    extensions: ['.js', '.json', '.jsx', '.mjs', '.ts', '.tsx', '.vue'],
   }
 })
