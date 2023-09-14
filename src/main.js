cdcb13a499985b2a31791f755d19e12e50cae390
Ciprian-hub <cipssul@gmail.com>
2023-09-14 10:31:15 +0300

    module

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-import { createApp } from '@vue/runtime-dom'
-import store from "./store"
-import router from "./router"
-import './index.css'
-import App from './App.vue'
-
-createApp(App)
-    .use(router)
-    .use(store)
-    .mount('#app')
+// import { createApp } from 'vue'
+// import store from "./store"
+// import router from "./router"
+// import './index.css'
+// import App from './App.vue'
+//
+// createApp(App)
+//     .use(router)
+//     .use(store)
+//     .mount('#app')
