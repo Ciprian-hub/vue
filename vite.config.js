bebc7c4d48f19869c90c9f33385d690d7e93b2dd
Ciprian-hub <cipssul@gmail.com>
2023-09-14 10:12:33 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
-    }
+    },
+    extensions: ['.js', '.json', '.jsx', '.mjs', '.ts', '.tsx', '.vue'],
   }
 })
