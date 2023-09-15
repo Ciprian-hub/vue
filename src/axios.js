39ee4f8155ce79d8090e722dd00f0e2fe102cdf5
Ciprian-hub <cipssul@gmail.com>
2023-09-15 20:53:09 +0300

    url

--- src/axios.js
+++ src/axios.js
@@ -3,7 +3,7 @@ import store from "./store";
 
 let axiosClient;
 axiosClient = axios.create({
-    baseURL: `${import.meta.env.BASE_URL}/api`
+    baseURL: `https://api-smartcoder.online/api`
 });
 axiosClient.interceptors.request.use(config => {
     config.headers.Authorization = `Bearer ${store.state.user.token}`
